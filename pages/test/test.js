ea238632b5601f26915f30de393f04d218b50f17
hanxiaoqing123 <1695722217@qq.com>
2017-12-28 22:06:33 +0800

    添加test页面

---
+++ pages/test/test.js
@@ -0,0 +1,26 @@
+Page({
+	data:{
+		name:"测试欢乐慧",
+	},
+	onLoad:function(options){
+		
+	},
+	onReady:function(){
+		
+	},
+	onShow:function(){
+		
+	},
+	onHide:function(){
+		
+	},
+	onUnload:function(){
+		
+	},
+	onPullDownRefresh:function(){
+		
+	},
+	onReachBottom:function(){
+		
+	}
+})		
