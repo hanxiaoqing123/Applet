c0066e07e45d56623dd687da9b56a9b822b3967f
hanxiaoqing123 <1695722217@qq.com>
2018-01-03 19:01:29 +0800

    事件  and 语法结构

---
+++ pages/demo3/demo3.js
@@ -0,0 +1,33 @@
+Page({
+	data:{
+		list:[
+		{"name":"tc01","address":"成都高新区天府三街银泰城"},
+		{"name":"tc02","address":"北京"},
+		{"name":"tc03","address":"上海"},
+		{"name":"tc04","address":"深圳"}
+		],
+		array:[1,2,3,4,5,6,7,8,9],
+		flag:false
+	},
+	onLoad:function(options){
+		
+	},
+	onReady:function(){
+		
+	},
+	onShow:function(){
+		
+	},
+	onHide:function(){
+		
+	},
+	onUnload:function(){
+		
+	},
+	onPullDownRefresh:function(){
+		
+	},
+	onReachBottom:function(){
+		
+	}
+})		
